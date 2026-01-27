378354ae0616d18d5a1e7a1a461d7edc26539861
lyhoangducuy <lyhoangducuy@gmail.com>
2026-01-27 14:57:41 +0700

    first commit

---
+++ taphoa2hand-backend/src/test/java/vn/edu/husc/taphoa2hand_backend/Taphoa2handBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.husc.taphoa2hand_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taphoa2handBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
